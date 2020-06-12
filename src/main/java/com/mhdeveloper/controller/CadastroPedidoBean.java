f515e9e33fe46e87a19a261eb3eb9f80951c1426
Marcos Hobson <marcoshobson@gmail.com>
2020-06-12 11:10:15 -0300

    Adicionando restrições no cadastro de pedido

--- src/main/java/com/mhdeveloper/controller/CadastroPedidoBean.java
+++ src/main/java/com/mhdeveloper/controller/CadastroPedidoBean.java
@@ -1,30 +1,41 @@
 package com.mhdeveloper.controller;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ViewScoped;
 import javax.inject.Named;
 
-import com.mhdeveloper.service.NegocioException;
+import com.mhdeveloper.model.EnderecoEntrega;
+import com.mhdeveloper.model.Pedido;
 
 @Named
-@RequestScoped
-public class CadastroPedidoBean {
+@ViewScoped
+public class CadastroPedidoBean implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	private Pedido pedido;
 	
 	private List<Integer> itens;
 	
 	public CadastroPedidoBean() {
+		pedido = new Pedido();
+		pedido.setEnderecoEntrega(new EnderecoEntrega());
 		itens = new ArrayList<>();
 		itens.add(1);
 	}
 	
 	public void salvar() {
-		throw new NegocioException("Pedido não pode ser salvo, pois ainda não foi implementado.");
 	}
 
 	public List<Integer> getItens() {
 		return itens;
+	}
+	
+	public Pedido getPedido() {
+		return pedido;
 	}
 
 }
--- src/main/java/com/mhdeveloper/model/Pedido.java
+++ src/main/java/com/mhdeveloper/model/Pedido.java
@@ -21,6 +21,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "pedido")
@@ -52,6 +53,7 @@ public class Pedido implements Serializable {
 		this.id = id;
 	}
 
+	@NotNull
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "data_criacao", nullable = false)
 	public Date getDataCriacao() {
@@ -71,6 +73,7 @@ public class Pedido implements Serializable {
 		this.observacao = observacao;
 	}
 
+	@NotNull
 	@Temporal(TemporalType.DATE)
 	@Column(name = "data_entrega", nullable = false)
 	public Date getDataEntrega() {
@@ -81,6 +84,7 @@ public class Pedido implements Serializable {
 		this.dataEntrega = dataEntrega;
 	}
 
+	@NotNull
 	@Column(name = "valor_frete", nullable = false, precision = 10, scale = 2)
 	public BigDecimal getValorFrete() {
 		return valorFrete;
@@ -90,6 +94,7 @@ public class Pedido implements Serializable {
 		this.valorFrete = valorFrete;
 	}
 
+	@NotNull
 	@Column(name = "valor_desconto", nullable = false, precision = 10, scale = 2)
 	public BigDecimal getValorDesconto() {
 		return valorDesconto;
@@ -99,6 +104,7 @@ public class Pedido implements Serializable {
 		this.valorDesconto = valorDesconto;
 	}
 
+	@NotNull
 	@Column(name = "valor_total", nullable = false, precision = 10, scale = 2)
 	public BigDecimal getValorTotal() {
 		return valorTotal;
@@ -108,6 +114,7 @@ public class Pedido implements Serializable {
 		this.valorTotal = valorTotal;
 	}
 
+	@NotNull
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false, length = 20)
 	public StatusPedido getStatus() {
@@ -118,6 +125,7 @@ public class Pedido implements Serializable {
 		this.status = status;
 	}
 
+	@NotNull
 	@Enumerated(EnumType.STRING)
 	@Column(name = "forma_pagamento", nullable = false, length = 20)
 	public FormaPagamento getFormaPagamento() {
@@ -128,6 +136,7 @@ public class Pedido implements Serializable {
 		this.formaPagamento = formaPagamento;
 	}
 
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "vendedor_id", nullable = false)
 	public Usuario getVendedor() {
@@ -138,6 +147,7 @@ public class Pedido implements Serializable {
 		this.vendedor = vendedor;
 	}
 
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "cliente_id", nullable = false)
 	public Cliente getCliente() {
