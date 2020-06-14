c507e75107179656c8b2f0e91975adce4b483a52
Marcos Hobson <marcoshobson@gmail.com>
2020-06-14 16:44:34 -0300

    Criando o produtor de Entity Manager e listando as categorias da base de dados

--- src/main/java/com/mhdeveloper/pedidovenda/controller/CadastroProdutoBean.java
+++ src/main/java/com/mhdeveloper/pedidovenda/controller/CadastroProdutoBean.java
@@ -1,10 +1,14 @@
 package com.mhdeveloper.pedidovenda.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
 
+import com.mhdeveloper.pedidovenda.model.Categoria;
 import com.mhdeveloper.pedidovenda.model.Produto;
 
 @Named
@@ -12,17 +16,32 @@ import com.mhdeveloper.pedidovenda.model.Produto;
 public class CadastroProdutoBean implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private EntityManager manager;
 
 	private Produto produto;
+	
+	private List<Categoria> categoriasPai;
 	
 	public CadastroProdutoBean() {
 		this.produto = new Produto();
 	}
 	
+	public void inicializar() {
+		System.out.println("Inicializando...");
+		
+		categoriasPai = manager.createQuery("from Categoria", Categoria.class).getResultList();
+	}
+	
 	public void salvar() {
-	};
+	}
 
 	public Produto getProduto() {
 		return produto;
+	}
+
+	public List<Categoria> getCategoriasPai() {
+		return categoriasPai;
 	}
 }
